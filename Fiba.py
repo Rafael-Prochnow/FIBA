63d1f6055e56dd1abc767c967c125e1945d65505
Rafael-Prochnow <rafaprochnow31@gmail.com>
2020-06-22 13:31:54 -0300

    p3

--- Fiba.py
+++ Fiba.py
@@ -19,28 +19,34 @@ jogada_jogada = soup.find(class_="selected-periods")
 # 2 - por tempo na quadra
 time_a = jogada_jogada.find_all(class_="action-item x--team-A")
 
-quarto_time_a = time_a.find(class_='period').get_text()
-tempo_time_a = time_a.find(class_='time').get_text()
-placar_time_a = time_a.find(class_='score-A x--bold').get_text()
-placar_time_b = time_a.find(class_='score-B').get_text()
-nome_time_a = time_a.find(class_='athlete-name')
-indicador_time_a = time_a.find(class_='athlete-name')
+quarto_time_a = [a.find(class_='period').get_text() for a in time_a]
+tempo_time_a = [a.find(class_='time').get_text() for a in time_a]
+placar_time_b = [a.find(class_='score-B').get_text() for a in time_a]
+placar_time_a = [a.find(class_='score-A').get_text() for a in time_a]
+# como ele retorna com \n eu preciso tirar por ; e como são três eu preciso apenas escolher o do meio ai uso strip
+acao = [a.find(class_="action").get_text() for a in time_a]
+organizar01 = [item.replace('\n', ';') for item in acao]
+indicador = [item.strip(';') for item in organizar01]
 
-print(quarto_time_a)
-print(tempo_time_a)
-print(placar_time_a)
-print(placar_time_b)
-print(nome_time_a)
-print(indicador_time_a)
-
-
+# duplica a coluna e faz uma coluna que use inidcador e
+print(indicador)
 '''
 dados = pd.DataFrame(
-    {'Quarto': quarto,
-     'Tempo': tempo,
-     'Time_01': time_site,
-     'Placar': placar,
-     'Inf_2': acao_pessoa02
+    {'Quarto': quarto_time_a,
+     'Tempo': tempo_time_a,
+     'Placar_casa': placar_time_a,
+     'Placar_visitante': placar_time_b,
+     'Inf_2': indicador
      })
+# separar nome e indicador
+separar_01 = dados['Inf_2'].str.split(";")
+nome = separar_01.str.get(1)
+indicador = separar_01.str.get(0)
+
+dados['Nomes'] = nome
+dados['Indicador'] = indicador
+dados.drop('Inf_2', axis=1, inplace=True)
+
+dados.to_csv("tabela_1.csv", index=None)
 '''
 
