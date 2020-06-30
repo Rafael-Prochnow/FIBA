920772e6217a030f9e4a5a1b48e39b865bbdabc3
Rafael-Prochnow <rafaprochnow31@gmail.com>
2020-06-30 10:51:44 -0300

    p8

--- Fiba.py
+++ Fiba.py
@@ -6,7 +6,7 @@ from selenium.webdriver.firefox.options import Options
 import requests
 from selenium.common.exceptions import NoSuchElementException
 
-r = requests.get('http://www.fiba.basketball/pt/basketballworldcup/2019/game/1409/Serbia-Czech-Republic')
+r = requests.get('http://www.fiba.basketball/pt/basketballworldcup/2019/game/1109/Australia-Czech-Republic')
 soup = BeautifulSoup(r.content, 'html.parser')
 jogada_jogada = soup.find(class_="selected-periods")
 
@@ -18,26 +18,48 @@ jogada_jogada = soup.find(class_="selected-periods")
 # 1 - por quarto
 # 2 - por tempo na quadra
 time_a = jogada_jogada.find_all(class_="action-item x--team-A")
-
-quarto_time_a = [a.find(class_='period').get_text() for a in time_a]
-tempo_time_a = [a.find(class_='time').get_text() for a in time_a]
-placar_time_b = [a.find(class_='score-B').get_text() for a in time_a]
-placar_time_a = [a.find(class_='score-A').get_text() for a in time_a]
+Aquarto_time_a = [a.find(class_='period').get_text() for a in time_a]
+Atempo_time_a = [a.find(class_='time').get_text() for a in time_a]
+Aplacar_time_b = [a.find(class_='score-B').get_text() for a in time_a]
+Aplacar_time_a = [a.find(class_='score-A').get_text() for a in time_a]
 # como ele retorna com \n eu preciso tirar por ; e como são três eu preciso apenas escolher o do meio ai uso strip
-acao = [a.find(class_="action").get_text() for a in time_a]
-organizar01 = [item.replace(';', '') for item in acao]
-organizar02 = [item.replace('\n', ';') for item in organizar01]
-indicador = [item.strip(';') for item in organizar02]
+Aacao = [a.find(class_="action").get_text() for a in time_a]
+Aorganizar01 = [item.replace(';', '') for item in Aacao]
+Aorganizar02 = [item.replace('\n', ';') for item in Aorganizar01]
+Aindicador = [item.strip(';') for item in Aorganizar02]
 
-dados = pd.DataFrame(
-    {'Quarto': quarto_time_a,
-     'Tempo': tempo_time_a,
-     'Placar_casa': placar_time_a,
-     'Placar_visitante': placar_time_b,
-     'Inf_2': indicador
+dadosA = pd.DataFrame(
+    {'Quarto': Aquarto_time_a,
+     'Tempo': Atempo_time_a,
+     'Placar_casa': Aplacar_time_a,
+     'Placar_visitante': Aplacar_time_b,
+     'Inf_2': Aindicador
+     })
+########################################################################################################################
+time_b = jogada_jogada.find_all(class_="action-item x--team-B")
+Bquarto_time_a = [a.find(class_='period').get_text() for a in time_b]
+Btempo_time_a = [a.find(class_='time').get_text() for a in time_b]
+Bplacar_time_b = [a.find(class_='score-B').get_text() for a in time_b]
+Bplacar_time_a = [a.find(class_='score-A').get_text() for a in time_b]
+# como ele retorna com \n eu preciso tirar por ; e como são três eu preciso apenas escolher o do meio ai uso strip
+Bacao = [a.find(class_="action").get_text() for a in time_b]
+Borganizar01 = [item.replace(';', '') for item in Bacao]
+Borganizar02 = [item.replace('\n', ';') for item in Borganizar01]
+Bindicador = [item.strip(';') for item in Borganizar02]
+
+dadosB = pd.DataFrame(
+    {'Quarto': Bquarto_time_a,
+     'Tempo': Btempo_time_a,
+     'Placar_casa': Bplacar_time_a,
+     'Placar_visitante': Bplacar_time_b,
+     'Inf_2': Bindicador
      })
 
+df_full = pd.concat([dadosA, dadosB], axis=0)
+df_full.to_csv("tabela_1.csv", index=None)
 
+
+'''
 saparar = dados["Inf_2"]
 
 # separar nome e indicador
@@ -101,44 +123,59 @@ a52 = a51.str.replace('steal', '1/BR')
 # assistencias
 a53 = a52.str.replace('made the assist', '1/AS')
 #5 Faltas
-a54 = a53.str.replace('unsportsmanlike foul 2 free throws awarded', '1/')
-a55 = a54.str.replace('personal foul', '1/')
-a56 = a55.str.replace(' 2 free throws awarded', '1/')
-a57 = a56.str.replace(' 1 free throw awarded', '1/')
-a58 = a57.str.replace('offensive foul', '1/')
-a59 = a58.str.replace('technical foul', '1/')
-a60 = a59.str.replace('foul drawn', '1/')
+a54 = a53.str.replace('unsportsmanlike foul 2 free throws awarded', '1/FC_A')
+a55 = a54.str.replace('personal foul', '1/FC_')
+a56 = a55.str.replace(' 2 free throws awarded', '')
+a57 = a56.str.replace(' 1 free throw awarded', '')
+a58 = a57.str.replace('offensive foul', '1/O')
+a59 = a58.str.replace('technical foul', '1/T')
+a60 = a59.str.replace('foul drawn', '1/FR')
 # substituição
-a61 = a60.str.replace('Substitution in', '1/')
-a62 = a61.str.replace('Substitution out', '1/')
+a61 = a60.str.replace('Substitution in', '1/substituicao_entra')
+a62 = a61.str.replace('Substitution out', '1/substituicao_sai')
 # erros
-a63 = a62.str.replace('turnover ball handling', '1/')
-a64 = a63.str.replace('turnover travelling', '1/')
-a65 = a64.str.replace('turnover bad pass', '1/')
-a66 = a65.str.replace('turnover 3 seconds violation', '1/')
-a67 = a66.str.replace('turnover out of bounds', '1/')
+a63 = a62.str.replace('turnover ball handling', '1/ER')
+a64 = a63.str.replace('turnover travelling', '1/ER')
+a65 = a64.str.replace('turnover bad pass', '1/ER')
+a66 = a65.str.replace('turnover 3 seconds violation', '1/ER')
+a67 = a66.str.replace('turnover out of bounds', '1/ER')
 # tocos
-a68 = a67.str.replace('blocked the shot', '1/')
+a68 = a67.str.replace('blocked the shot', '1/TO')
 # tempo técnico
-a69 = a68.str.replace('Timeout', '1/')
+a69 = a68.str.replace('Timeout', '1/tempo_tecnico')
 # cravada
-a70 = a69.str.replace('Enterrada', '1/')
+a70 = a69.str.replace('Enterrada', '1/EN')
 # ponte aerea
-a71 = a70.str.replace('Ponte Aerea Errada', '1/')
-a72 = a71.str.replace('Ponte Aerea Certa', '1/')
+a71 = a70.str.replace('Ponte Aerea Errada', '1/EN_T')
+a72 = a71.str.replace('Ponte Aerea Certa', '1/EN')
 # bola no alto
-a73 = a72.str.replace('jump ball won', '1/')
-a74 = a73.str.replace('jump ball situation throw-in', '1/')
+a73 = a72.str.replace('jump ball won', '1/inicio_quarto')
+a74 = a73.str.replace('jump ball situation throw-in', '1/muda_bola')
 # não sei
-a75 = a74.str.replace('Tip In Errada', '1/')
-a76 = a75.str.replace('Tip In Certa', '1/')
+a75 = a74.str.replace('Tip In Errada', '1/2_Pts_T')
+a76 = a75.str.replace('Tip In Certa', '1/2_Pts_C')
 a77 = a76.str.replace('team ', '')
-a78 = a77.str.replace('layup blocked', '1/')
-a79 = a78.str.replace('free throw made', '1/')
+a78 = a77.str.replace('layup blocked', '1/2_Pts_T')
+a79 = a78.str.replace('free throw made', '1/LL_Pts_T')
+a80 = a79.str.replace('turnover', '1/ER')
+a81 = a80.str.replace('2pt step back jump shot made', '1/2_Pts_C')
+a82 = a81.str.replace('2pt step back jump shot missed', '1/2_Pts_T')
+a83 = a82.str.replace('2pt step back jump shot blocked', '1/2_Pts_T')
 
-dados['Nomes'] = a79
+# nome;1/indicador
+# estou fazendo isso pq tem indicadores que não tem jogadores(noomes)
+# quando coloco 1 ele ajuda a separa os dois e deixar uma variável 1 quando não tem nome
+# depois eu tiro esse ;1 e coloco um identificador do time
+mudados_00 = a83.str.split('/')
+mudados_01 = mudados_00.str.get(1)
+dados['Indicador'] = mudados_01
+
+mudados_02 = mudados_00.str.get(0)
+mudados_03 = mudados_02.str.replace('1', '')
+mudados_04 = mudados_03.str.replace(';', '')
+dados['Nome'] = mudados_04
+
 dados.drop('Inf_2', axis=1, inplace=True)
 dados.to_csv("tabela_1.csv", index=None)
-
-
+'''
 
